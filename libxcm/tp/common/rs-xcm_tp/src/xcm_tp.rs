158be742351c4806238354ed4e6c0c4a0f21a2d4
Johan Hummel <johan.c.hummel@gmail.com>
2025-04-29 01:01:29 +0200

    Update xcm_tp.rs to use the common rust lib

--- libxcm/tp/common/rs-xcm_tp/src/xcm_tp.rs
+++ libxcm/tp/common/rs-xcm_tp/src/xcm_tp.rs
@@ -4,165 +4,21 @@
     non_upper_case_globals,
     clippy::missing_safety_doc
 )]
-#![feature(extern_types)]
-unsafe extern "C" {
-    pub type ctl;
-    pub type xpoll;
-    pub type attr_tree;
-    fn __errno_location() -> *mut libc::c_int;
-    fn xcm_set_blocking(socket: *mut xcm_socket, should_block: bool) -> libc::c_int;
-    fn xcm_remote_addr(conn_socket: *mut xcm_socket) -> *const libc::c_char;
-    fn xcm_local_addr(socket: *mut xcm_socket) -> *const libc::c_char;
-    fn attr_tree_add_value_node(
-        tree: *mut attr_tree,
-        path: *const libc::c_char,
-        s: *mut xcm_socket,
-        context: *mut libc::c_void,
-        type_0: xcm_attr_type,
-        set: attr_set,
-        get: attr_get,
-    );
-    fn abort() -> !;
-    fn memcpy(
-        _: *mut libc::c_void,
-        _: *const libc::c_void,
-        _: libc::c_ulong,
-    ) -> *mut libc::c_void;
-    fn strcpy(_: *mut libc::c_char, _: *const libc::c_char) -> *mut libc::c_char;
-    fn strcmp(_: *const libc::c_char, _: *const libc::c_char) -> libc::c_int;
-    fn strlen(_: *const libc::c_char) -> libc::c_ulong;
-    fn ut_calloc(size: size_t) -> *mut libc::c_void;
-    fn ut_free(ptr: *mut libc::c_void);
-    fn log_console_conf(enabled: bool);
-    fn log_is_enabled(type_0: log_type) -> bool;
-    fn __log_event(
-        type_0: log_type,
-        file: *const libc::c_char,
-        line: libc::c_int,
-        function: *const libc::c_char,
-        s: *mut xcm_socket,
-        format: *const libc::c_char,
-        _: ...
-    );
-    fn xcm_addr_parse_proto(
-        addr_s: *const libc::c_char,
-        proto: *mut libc::c_char,
-        capacity: size_t,
-    ) -> libc::c_int;
-    fn ctl_create(socket: *mut xcm_socket) -> *mut ctl;
-    fn ctl_destroy(ctl: *mut ctl, owner: bool);
-    fn ctl_process(ctl: *mut ctl);
-}
-pub type size_t = libc::c_ulong;
-pub type __int64_t = libc::c_long;
-pub type __uint64_t = libc::c_ulong;
-pub type int64_t = __int64_t;
+
+use xcm_rust_common::*;
+
+unsafe extern "C" { fn attr_tree_add_value_node(
+            tree: *mut attr_tree,
+            path: *const libc::c_char,
+            s: *mut xcm_socket,
+            context: *mut libc::c_void,
+            type_0: xcm_attr_type,
+            set: attr_set,
+            get: attr_get,
+        );
+    }
 
 
-pub type xcm_attr_type = libc::c_uint;
-pub const xcm_attr_type_double: xcm_attr_type = 5;
-pub const xcm_attr_type_bin: xcm_attr_type = 4;
-pub const xcm_attr_type_str: xcm_attr_type = 3;
-pub const xcm_attr_type_int64: xcm_attr_type = 2;
-pub const xcm_attr_type_bool: xcm_attr_type = 1;
-pub type uint64_t = __uint64_t;
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct xcm_socket {
-    pub proto: *const xcm_tp_proto,
-    pub type_0: xcm_socket_type,
-    pub sock_id: int64_t,
-    pub auto_enable_ctl: bool,
-    pub auto_update: bool,
-    pub is_blocking: bool,
-    pub xpoll: *mut xpoll,
-    pub condition: libc::c_int,
-    pub ctl: *mut ctl,
-    pub skipped_ctl_calls: uint64_t,
-}
-pub type xcm_socket_type = libc::c_uint;
-pub const xcm_socket_type_server: xcm_socket_type = 1;
-pub const xcm_socket_type_conn: xcm_socket_type = 0;
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct xcm_tp_proto {
-    pub name: [libc::c_char; 33],
-    pub ops: *const xcm_tp_ops,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct xcm_tp_ops {
-    pub init: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *mut xcm_socket) -> libc::c_int,
-    >,
-    pub connect: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *const libc::c_char) -> libc::c_int,
-    >,
-    pub server: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *const libc::c_char) -> libc::c_int,
-    >,
-    pub close: Option::<unsafe extern "C" fn(*mut xcm_socket) -> ()>,
-    pub cleanup: Option::<unsafe extern "C" fn(*mut xcm_socket) -> ()>,
-    pub accept: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *mut xcm_socket) -> libc::c_int,
-    >,
-    pub send: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *const libc::c_void, size_t) -> libc::c_int,
-    >,
-    pub receive: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *mut libc::c_void, size_t) -> libc::c_int,
-    >,
-    pub update: Option::<unsafe extern "C" fn(*mut xcm_socket) -> ()>,
-    pub finish: Option::<unsafe extern "C" fn(*mut xcm_socket) -> libc::c_int>,
-    pub get_transport: Option::<
-        unsafe extern "C" fn(*mut xcm_socket) -> *const libc::c_char,
-    >,
-    pub get_remote_addr: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, bool) -> *const libc::c_char,
-    >,
-    pub get_local_addr: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, bool) -> *const libc::c_char,
-    >,
-    pub set_local_addr: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *const libc::c_char) -> libc::c_int,
-    >,
-    pub max_msg: Option::<unsafe extern "C" fn(*mut xcm_socket) -> size_t>,
-    pub get_cnt: Option::<unsafe extern "C" fn(*mut xcm_socket, xcm_tp_cnt) -> int64_t>,
-    pub enable_ctl: Option::<unsafe extern "C" fn(*mut xcm_socket) -> ()>,
-    pub attr_populate: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *mut attr_tree) -> (),
-    >,
-    pub priv_size: Option::<unsafe extern "C" fn(xcm_socket_type) -> size_t>,
-}
-pub type xcm_tp_cnt = libc::c_uint;
-pub const xcm_tp_cnt_from_lower_msgs: xcm_tp_cnt = 7;
-pub const xcm_tp_cnt_to_lower_msgs: xcm_tp_cnt = 6;
-pub const xcm_tp_cnt_from_app_msgs: xcm_tp_cnt = 5;
-pub const xcm_tp_cnt_to_app_msgs: xcm_tp_cnt = 4;
-pub const xcm_tp_cnt_from_lower_bytes: xcm_tp_cnt = 3;
-pub const xcm_tp_cnt_to_lower_bytes: xcm_tp_cnt = 2;
-pub const xcm_tp_cnt_from_app_bytes: xcm_tp_cnt = 1;
-pub const xcm_tp_cnt_to_app_bytes: xcm_tp_cnt = 0;
-pub type attr_set = Option::<
-    unsafe extern "C" fn(
-        *mut xcm_socket,
-        *mut libc::c_void,
-        *const libc::c_void,
-        size_t,
-    ) -> libc::c_int,
->;
-pub type attr_get = Option::<
-    unsafe extern "C" fn(
-        *mut xcm_socket,
-        *mut libc::c_void,
-        *mut libc::c_void,
-        size_t,
-    ) -> libc::c_int,
->;
-pub type log_type = libc::c_uint;
-pub const log_type_error: log_type = 1;
-pub const log_type_debug: log_type = 0;
-pub type C2RustUnnamed = libc::c_uint;
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn xcm_tp_socket_type_name(
@@ -191,31 +47,6 @@ unsafe extern "C" fn get_next_sock_id() -> int64_t {
 
 }
 
-// #[unsafe(no_mangle)]
-// pub unsafe extern "C" fn xcm_tp_socket_create(
-//     mut proto: *const xcm_tp_proto,
-//     mut type_0: xcm_socket_type,
-//     mut xpoll: *mut xpoll,
-//     mut auto_enable_ctl: bool,
-//     mut auto_update: bool,
-//     mut is_blocking: bool,
-// ) -> *mut xcm_socket {
-//     let mut priv_size: size_t = ((*(*proto).ops).priv_size)
-//         .expect("non-null function pointer")(type_0);
-//     let mut s: *mut xcm_socket = ut_calloc(
-//         (::core::mem::size_of::<xcm_socket>() as libc::c_ulong).wrapping_add(priv_size),
-//     ) as *mut xcm_socket;
-//     (*s).proto = proto;
-//     (*s).type_0 = type_0;
-//     (*s).auto_enable_ctl = auto_enable_ctl;
-//     (*s).auto_update = auto_update;
-//     (*s).is_blocking = is_blocking;
-//     (*s).xpoll = xpoll;
-//     (*s).sock_id = get_next_sock_id();
-//     (*s).condition = 0 as libc::c_int;
-//     (*s).ctl = 0 as *mut ctl;
-//     return s;
-// }
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn xcm_tp_socket_create(
     proto: *const xcm_tp_proto,
@@ -232,6 +63,7 @@ pub unsafe extern "C" fn xcm_tp_socket_create(
         (core::mem::size_of::<xcm_socket>() as libc::c_ulong)
             .wrapping_add(priv_size),
     ).cast::<xcm_socket>();
+    // let s = Box::into_raw(Box::new(core::mem::zeroed::<xcm_socket>()));
 
     *s = xcm_socket {
         proto,
@@ -254,6 +86,7 @@ pub unsafe extern "C" fn xcm_tp_socket_create(
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn xcm_tp_socket_destroy(s: *mut xcm_socket) { unsafe {
     ut_free(s as *mut libc::c_void); //keep ut_free? Think the only other option is libc::free(s as *mut libc::c_void);, but don't know if that is better
+    // let s = Box::from_raw(s);
 }}
 
 #[unsafe(no_mangle)]
