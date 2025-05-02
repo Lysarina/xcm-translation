7776d344818c6ac6ea27bf06c51a3b4be4d2b0fe
Ella Viirola <ella.viirola@outlook.com>
2025-05-02 13:10:01 +0200

    Rs log wip

--- libxcm/core/rs-log/src/log.rs
+++ libxcm/core/rs-log/src/log.rs
@@ -6,6 +6,8 @@
 )]
 #![feature(c_variadic, extern_types)]
 
+use xcm_rust_common::xcm_tp::xcm_socket;
+
 use std::sync::atomic::{AtomicBool, Ordering};
 unsafe extern "C" {
     pub type ctl;
@@ -49,88 +51,13 @@ pub struct __va_list_tag {
     pub overflow_arg_area: *mut libc::c_void,
     pub reg_save_area: *mut libc::c_void,
 }
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct xcm_socket {
-    pub proto: *const xcm_tp_proto,
-    pub type_0: xcm_socket_type,
-    pub sock_id: int64_t,
-    pub auto_enable_ctl: bool,
-    pub auto_update: bool,
-    pub is_blocking: bool,
-    pub xpoll: *mut xpoll,
-    pub condition: libc::c_int,
-    pub ctl: *mut ctl,
-    pub skipped_ctl_calls: uint64_t,
-}
+
 pub type uint64_t = __uint64_t;
 pub type __uint64_t = libc::c_ulong;
 pub type int64_t = __int64_t;
 pub type __int64_t = libc::c_long;
-pub type xcm_socket_type = libc::c_uint;
-pub const xcm_socket_type_server: xcm_socket_type = 1;
-pub const xcm_socket_type_conn: xcm_socket_type = 0;
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct xcm_tp_proto {
-    pub name: [libc::c_char; 33],
-    pub ops: *const xcm_tp_ops,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct xcm_tp_ops {
-    pub init: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *mut xcm_socket) -> libc::c_int,
-    >,
-    pub connect: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *const libc::c_char) -> libc::c_int,
-    >,
-    pub server: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *const libc::c_char) -> libc::c_int,
-    >,
-    pub close: Option::<unsafe extern "C" fn(*mut xcm_socket) -> ()>,
-    pub cleanup: Option::<unsafe extern "C" fn(*mut xcm_socket) -> ()>,
-    pub accept: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *mut xcm_socket) -> libc::c_int,
-    >,
-    pub send: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *const libc::c_void, size_t) -> libc::c_int,
-    >,
-    pub receive: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *mut libc::c_void, size_t) -> libc::c_int,
-    >,
-    pub update: Option::<unsafe extern "C" fn(*mut xcm_socket) -> ()>,
-    pub finish: Option::<unsafe extern "C" fn(*mut xcm_socket) -> libc::c_int>,
-    pub get_transport: Option::<
-        unsafe extern "C" fn(*mut xcm_socket) -> *const libc::c_char,
-    >,
-    pub get_remote_addr: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, bool) -> *const libc::c_char,
-    >,
-    pub get_local_addr: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, bool) -> *const libc::c_char,
-    >,
-    pub set_local_addr: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *const libc::c_char) -> libc::c_int,
-    >,
-    pub max_msg: Option::<unsafe extern "C" fn(*mut xcm_socket) -> size_t>,
-    pub get_cnt: Option::<unsafe extern "C" fn(*mut xcm_socket, xcm_tp_cnt) -> int64_t>,
-    pub enable_ctl: Option::<unsafe extern "C" fn(*mut xcm_socket) -> ()>,
-    pub attr_populate: Option::<
-        unsafe extern "C" fn(*mut xcm_socket, *mut attr_tree) -> (),
-    >,
-    pub priv_size: Option::<unsafe extern "C" fn(xcm_socket_type) -> size_t>,
-}
+
 pub type size_t = libc::c_ulong;
-pub type xcm_tp_cnt = libc::c_uint;
-pub const xcm_tp_cnt_from_lower_msgs: xcm_tp_cnt = 7;
-pub const xcm_tp_cnt_to_lower_msgs: xcm_tp_cnt = 6;
-pub const xcm_tp_cnt_from_app_msgs: xcm_tp_cnt = 5;
-pub const xcm_tp_cnt_to_app_msgs: xcm_tp_cnt = 4;
-pub const xcm_tp_cnt_from_lower_bytes: xcm_tp_cnt = 3;
-pub const xcm_tp_cnt_to_lower_bytes: xcm_tp_cnt = 2;
-pub const xcm_tp_cnt_from_app_bytes: xcm_tp_cnt = 1;
-pub const xcm_tp_cnt_to_app_bytes: xcm_tp_cnt = 0;
 pub type log_type = libc::c_uint;
 pub const log_type_error: log_type = 1;
 pub const log_type_debug: log_type = 0;
@@ -220,7 +147,6 @@ unsafe extern "C" fn log_console(
     mut ap: ::core::ffi::VaList,
 ) { unsafe {
     if console_enabled.load(Ordering::Relaxed) {
-    // if ::core::intrinsics::atomic_load_relaxed(&raw mut console_enabled) {
         let mut _oerrno: libc::c_int = *__errno_location();
         let mut buf: [libc::c_char; 8192] = [0; 8192];
         format_msg(
@@ -241,7 +167,6 @@ unsafe extern "C" fn log_console(
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn log_is_enabled(mut _type_0: log_type) -> bool {
     if console_enabled.load(Ordering::Relaxed) {
-    // if ::core::intrinsics::atomic_load_relaxed(&raw mut console_enabled) {
         return 1 as libc::c_int != 0;
     }
     0 as libc::c_int != 0
@@ -263,5 +188,4 @@ pub unsafe extern "C" fn __log_event(
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn log_console_conf(enabled: bool) {
     console_enabled.store(enabled, Ordering::Relaxed);
-    // ::core::intrinsics::atomic_store_relaxed(&console_enabled as *mut bool, enabled);
 }
